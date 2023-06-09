a3cfede5754d7df1ecb77e0e33a9e4bee2906a5b
Yifei Dong <yifeid3@gmail.com>
2023-06-09 13:42:07 +0930

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by USM Admin on 9/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
